66d4cabbb3fa67f41f4e18a57d78e3342d4df0f2
Lukas Schreiner <dev@lschreiner.de>
2020-02-02 12:31:38 +0100

    KC 2020: Prepare labels, texts for translations

--- kcevent/models.py
+++ kcevent/models.py
@@ -7,14 +7,14 @@ class KCPerson(models.Model):
         verbose_name = _('Person')
         verbose_name_plural = _('Persons')
 
-    first_name = models.CharField(max_length=120)
-    last_name = models.CharField(max_length=120)
-    street = models.CharField(max_length=120)
-    house_number = models.CharField(max_length=10)
-    city = models.CharField(max_length=120)
-    zip_code = models.CharField(max_length=10)
-    phone = models.CharField(max_length=50, blank=True)
-    mail_addr = models.EmailField()
+    first_name = models.CharField(max_length=120, verbose_name=_('First name'))
+    last_name = models.CharField(max_length=120, verbose_name=_('Surname'))
+    street = models.CharField(max_length=120, verbose_name=_('Street'))
+    house_number = models.CharField(max_length=10, verbose_name=_('House no.'))
+    city = models.CharField(max_length=120, verbose_name=_('City'))
+    zip_code = models.CharField(max_length=10, verbose_name=_('Postal code'))
+    phone = models.CharField(max_length=50, blank=True, verbose_name=_('Phone'))
+    mail_addr = models.EmailField(verbose_name=_('Mail address'))
 
     def __str__(self):
         return '{0} {1}'.format(self.first_name, self.last_name)
@@ -39,22 +39,24 @@ class Participant(KCPerson):
         GENDER_FEMALE = 'W', _('Female')
         GENDER_DIVERT = 'D', _('Divert')
 
-    birthday = models.DateField()
-    church = models.ForeignKey('Partner', null=True, on_delete=models.SET_NULL)
-    intolerances = models.TextField(blank=True)
+    birthday = models.DateField(verbose_name=_('Birthday'))
+    church = models.ForeignKey('Partner', null=True, on_delete=models.SET_NULL, verbose_name=_('Church'))
+    intolerances = models.TextField(blank=True, verbose_name=_('Intolerances'))
     nutrition = models.CharField(
         max_length=3,
         choices=NutritionTypes.choices,
     )
-    lactose_intolerance = models.BooleanField(default=False)
-    celiac_disease = models.BooleanField(default=False)
+    lactose_intolerance = models.BooleanField(default=False, verbose_name=_('Lactose intolerance'))
+    celiac_disease = models.BooleanField(default=False, verbose_name=_('Celiac disease'))
     role = models.CharField(
         max_length=2,
         choices=ParticipantRoles.choices,
+        verbose_name=_('Role')
     )
     gender = models.CharField(
         max_length=1,
         choices=GenderTypes.choices,
+        verbose_name=_('Gender')
     )
 
 class Partner(models.Model):
@@ -62,20 +64,21 @@ class Partner(models.Model):
         verbose_name = _('Partner')
         verbose_name_plural = _('Partner')
 
-    name = models.CharField(max_length=250)
-    street = models.CharField(max_length=120)
-    house_number = models.CharField(max_length=10)
-    city = models.CharField(max_length=120)
-    zip_code = models.CharField(max_length=10)
-    mail_church = models.EmailField()
+    name = models.CharField(max_length=250, verbose_name=_('Name'))
+    street = models.CharField(max_length=120, verbose_name=_('Street'))
+    house_number = models.CharField(max_length=10, verbose_name=_('House no.'))
+    city = models.CharField(max_length=120, verbose_name=_('City'))
+    zip_code = models.CharField(max_length=10, verbose_name=_('Postal code'))
+    mail_church = models.EmailField(verbose_name=_('Mail address'))
 
-    representative = models.ForeignKey(KCPerson, on_delete=models.CASCADE, related_name='+')
-    contact_person = models.ForeignKey(KCPerson, on_delete=models.CASCADE, related_name='+')
+    representative = models.ForeignKey(KCPerson, on_delete=models.CASCADE, related_name='+', verbose_name=_('Responsible person'))
+    contact_person = models.ForeignKey(KCPerson, on_delete=models.CASCADE, related_name='+', verbose_name=_('Contact person'))
 
     events = models.ManyToManyField(
         'KCEvent',
         through='KCEventPartner',
         through_fields=('evp_partner', 'evp_event'),
+        verbose_name=_('Events')
     )
 
     def __str__(self):
@@ -86,18 +89,22 @@ class KCEvent(models.Model):
         verbose_name = _('Event')
         verbose_name_plural = _('Events')
 
-    name = models.CharField(max_length=250)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    registration_start = models.DateField(null=True)
-    registration_end = models.DateField(null=True)
-    event_url = models.SlugField()
-    reg_pwd = models.CharField(max_length=250, blank=True)
+    name = models.CharField(max_length=250, verbose_name=_('Name'))
+    start_date = models.DateField(verbose_name=_('Start date'))
+    end_date = models.DateField(verbose_name=_('End date'))
+    registration_start = models.DateField(null=True, verbose_name=_('Registration start'))
+    registration_end = models.DateField(null=True, verbose_name=_('Registration end'))
+    event_url = models.SlugField(verbose_name=_('Registration URL'), help=_('SEO optimized location for register to event.'))
+    reg_pwd = models.CharField(
+        max_length=250, blank=True, verbose_name=_('Registration password'), 
+        help=_('Password (can be empty) which is necessary in order to be able to register to event.')
+    )
 
     participants = models.ManyToManyField(
         'Participant',
         through='KCEventRegistration',
         through_fields=('reg_event', 'reg_user'),
+        verbose_name=_('Participants')
     )
 
     def __str__(self):
@@ -145,20 +152,20 @@ class KCEventPartner(models.Model):
         verbose_name = _('Event Partner')
         verbose_name_plural = _('Event Partners')
 
-    evp_event = models.ForeignKey(KCEvent, on_delete=models.CASCADE)
-    evp_partner = models.ForeignKey(Partner, on_delete=models.CASCADE)
+    evp_event = models.ForeignKey(KCEvent, on_delete=models.CASCADE, verbose_name=_('Event'))
+    evp_partner = models.ForeignKey(Partner, on_delete=models.CASCADE, verbose_name=_('Event partner'))
     # contract
-    evp_doc_contract = models.FileField(upload_to=getUploadPathEventPartner, null=True)
+    evp_doc_contract = models.FileField(upload_to=getUploadPathEventPartner, null=True, verbose_name=_('Contract'))
     # statistics
     # Participants
-    evp_apx_participant_m = models.PositiveSmallIntegerField(default=0)
-    evp_apx_participant_w = models.PositiveSmallIntegerField(default=0)
+    evp_apx_participant_m = models.PositiveSmallIntegerField(default=0, verbose_name=_('Approx. no. of male par.'))
+    evp_apx_participant_w = models.PositiveSmallIntegerField(default=0, verbose_name=_('Approx. no. of female par.'))
     # Reloaded
-    evp_apx_reloaded_m = models.PositiveSmallIntegerField(default=0)
-    evp_apx_reloaded_w = models.PositiveSmallIntegerField(default=0)
+    evp_apx_reloaded_m = models.PositiveSmallIntegerField(default=0, verbose_name=_('Approx. no. of male reloaded'))
+    evp_apx_reloaded_w = models.PositiveSmallIntegerField(default=0, verbose_name=_('Approx. no. of female reloaded'))
     # Member
-    evp_apx_member_m = models.PositiveSmallIntegerField(default=0)
-    evp_apx_member_w = models.PositiveSmallIntegerField(default=0)
+    evp_apx_member_m = models.PositiveSmallIntegerField(default=0, verbose_name=_('Approx. no. of male members'))
+    evp_apx_member_w = models.PositiveSmallIntegerField(default=0, verbose_name=_('Approx. no. of female members'))
 
     def __str__(self):
         # event ?
@@ -179,15 +186,15 @@ class KCEventRegistration(models.Model):
         verbose_name = _('Event registration')
         verbose_name_plural = _('Event registrations')
 
-    reg_time = models.DateTimeField(auto_now_add=True)
-    reg_event = models.ForeignKey(KCEvent, on_delete=models.CASCADE)
-    reg_user = models.ForeignKey(Participant, on_delete=models.CASCADE)
-    reg_notes = models.TextField(blank=True)
+    reg_time = models.DateTimeField(auto_now_add=True, verbose_name=_('Registration time'))
+    reg_event = models.ForeignKey(KCEvent, on_delete=models.CASCADE, verbose_name=_('Event'))
+    reg_user = models.ForeignKey(Participant, on_delete=models.CASCADE, verbose_name=_('Person'))
+    reg_notes = models.TextField(blank=True, verbose_name=_('Notes'))
 
     # further documentation
-    reg_doc_pass = models.FileField(upload_to=getUploadPathEventRegistration)
-    reg_doc_meddispense = models.FileField(upload_to=getUploadPathEventRegistration, null=True, blank=True)
-    reg_doc_consent = models.FileField(upload_to=getUploadPathEventRegistration)
+    reg_doc_pass = models.FileField(upload_to=getUploadPathEventRegistration, verbose_name=_('Event passport'))
+    reg_doc_meddispense = models.FileField(upload_to=getUploadPathEventRegistration, null=True, blank=True, verbose_name=_('Medical dispense'))
+    reg_doc_consent = models.FileField(upload_to=getUploadPathEventRegistration, verbose_name=_('Consent form'))
 
     def __str__(self):
         # event ?
