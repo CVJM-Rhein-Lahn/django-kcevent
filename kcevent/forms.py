281dff2e4fb198a1a8632c5799fc66defba9799d
Lukas Schreiner <dev@lschreiner.de>
2020-02-11 20:43:48 +0100

    KC 2020: Added placeholder for forms

--- kcevent/forms.py
+++ kcevent/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from kcevent.models import Participant, KCEventRegistration
+from django.utils.translation import ugettext_lazy as _
 
 class ParticipantForm(forms.ModelForm):
     class Meta:
@@ -10,8 +11,23 @@ class ParticipantForm(forms.ModelForm):
             'role', 'gender', 'lactose_intolerance', 'celiac_disease'
         ]
         widgets = {
-            'birthday': forms.DateInput(attrs={'type': 'date'})
+            'first_name': forms.TextInput(attrs={'placeholder': _('First name')}),
+            'last_name': forms.TextInput(attrs={'placeholder': _('Surname')}),
+            'street': forms.TextInput(attrs={'placeholder': _('Street')}),
+            'house_number': forms.TextInput(attrs={'placeholder': _('House no.')}),
+            'zip_code': forms.TextInput(attrs={'placeholder': _('Postal code')}),
+            'city': forms.TextInput(attrs={'placeholder': _('City')}),
+            'phone': forms.TextInput(attrs={'placeholder': _('Phone')}),
+            'mail_addr': forms.TextInput(attrs={'placeholder': _('Mail address')}),
+            'birthday': forms.DateInput(attrs={'type': 'date'}),
+            'intolerances': forms.Textarea(attrs={'placeholder': _('Allergies / intolerances')}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['church'].empty_label = _('Please choose your church')
+        # replace empty choice
+        self.fields['role'].widget.choices = [('', _('Please choose your role'))] + self.fields['role'].widget.choices[1:]
 
 class KCEventRegistrationForm(forms.ModelForm):
     class Meta:
@@ -20,4 +36,7 @@ class KCEventRegistrationForm(forms.ModelForm):
             'reg_doc_pass', 'reg_doc_meddispense', 'reg_doc_consent', 'reg_notes',
             'reg_consent'
         ]
+        widgets = {
+            'reg_notes': forms.Textarea(attrs={'placeholder': _('Other communications')}),
+        }
 
